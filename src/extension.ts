0826ae4d7471c30629c91c6f6399c9370bb057df
egamma <egamma@microsoft.com>
2024-04-01 12:19:05 +0200

    Introduce VisionChatResult class

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,7 @@ import * as crypto from 'crypto';
 import * as dotenv from 'dotenv';
 import sharp from 'sharp';
 import OpenAI from 'openai';
+import { text } from 'stream/consumers';
 
 const VISION_PARTICIPANT_ID = 'vision-participant.particpant';
 const CHAT_ABOUT_IMAGE_COMMAND_ID = 'vision-participant.chatCommand';
@@ -27,10 +28,6 @@ const SYSTEM_MESSAGE =
 	`1. Use Markdown formatting in your answers.\n` +
 	`2. Make sure to include the programming language name at the start of the Markdown code blocks.\n` +
 	`3. Avoid wrapping the whole response in triple backticks.\n`;
-
-interface VisionChatResult extends vscode.ChatResult {
-	result: string;
-}
 
 // Enable that the OPENAI_API_KEY can also be set in ~/.env
 dotenv.config({ path: `${os.homedir()}/.env` });
@@ -124,6 +121,43 @@ class ChatImage {
 	}
 }
 
+interface VisionChatResult extends vscode.ChatResult {
+	result: string;
+}
+
+class VisionChatCodeBlocks {
+	private codeBlocks: string[] = [];
+
+	constructor(public response: string) {
+		this.codeBlocks = this.extractAllMarkdownCodeBlocks(response);
+	}
+
+	numberOfCodeBlocks(): number {
+		return this.codeBlocks.length;
+	}
+
+	getBlockLanguage(index: number): string | null {
+		let codeBlock = this.codeBlocks[index];
+		const languageRegex = /```(\S*)[\s\S]*?```/;
+		const match = codeBlock.match(languageRegex);
+		return match && match[1] ? match[1] : null;
+	}
+
+	getPlainBlock(index: number): string {
+		let block = this.codeBlocks[index];
+		const lines = block.split('\n');
+		lines.shift();
+		lines.pop();
+		return lines.join('\n');
+	}
+
+	private extractAllMarkdownCodeBlocks(markdown: string): string[] {
+		const codeBlockRegex = /```[\s\S]*?```/g;
+		const codeBlocks = markdown.match(codeBlockRegex);
+		return codeBlocks || [];
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	const handler: vscode.ChatRequestHandler = async (request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<VisionChatResult> => {
@@ -178,18 +212,15 @@ export function activate(context: vscode.ExtensionContext) {
 			stream.markdown(delta);
 		}
 
-		// show preview button if there is a single html code block
 		if (responseText.length > 0) {
-			let codeBlocks = extractAllMarkdownCodeBlocks(responseText);
-			if (codeBlocks.length === 1) {
-				let language = getLanguageFromMarkdownCodeBlock(codeBlocks[0]);
-				if (language === 'html') {
-					stream.button({
-						command: PREVIEW_COMMAND_ID,
-						arguments: [codeBlocks[0]],
-						title: vscode.l10n.t('Preview')
-					});
-				}
+			let codeBlocks = new VisionChatCodeBlocks(responseText);
+			// show preview button if there is a single html code block
+			if (codeBlocks.numberOfCodeBlocks() === 1 && codeBlocks.getBlockLanguage(0) === 'html') {
+				stream.button({
+					command: PREVIEW_COMMAND_ID,
+					arguments: [codeBlocks.getPlainBlock(0)],
+					title: vscode.l10n.t('Preview')
+				});
 			}
 		}
 		return { result: responseText };
@@ -204,9 +235,13 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand(PREVIEW_COMMAND_ID, showPreview),
 	);
 
+	/*
+	* Commands
+	*/
 	async function showPreview(arg: string): Promise<void> {
-		let htmlSource = removeFirstAndLastLine(arg);
-		// TODO Hack
+		let htmlSource =arg;
+
+		// TODO Hack for creating a temporary file and opening it in the browser
 		let workspacePath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 		const filePath = path.join(workspacePath!, 'preview.html');
 		await fs.writeFile(filePath, htmlSource);
@@ -214,11 +249,29 @@ export function activate(context: vscode.ExtensionContext) {
 		await vscode.commands.executeCommand('vscode.open', uri);
 		// using https://marketplace.visualstudio.com/items?itemName=ms-vscode.live-server
 		await vscode.commands.executeCommand('livePreview.start.preview.atFile');
+
 		// using live server extension: https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
 		// await vscode.commands.executeCommand('extension.liveServer.goOnline');
 	}
 
 	async function chatAboutImage(): Promise<void> {
+
+		function getFilePathFromEditor(): string | undefined {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				return editor.document.uri.fsPath;
+			}
+			// The editors showing an image file are custom editors. Therefore
+			// get the file path from the active tab
+			let tab = vscode.window.tabGroups.activeTabGroup.activeTab;
+			if (tab) {
+				if (tab.input instanceof vscode.TabInputCustom) {
+					return tab.input.uri.fsPath;
+				}
+			}
+			return undefined
+		}
+
 		let filePath = getFilePathFromEditor();
 
 		const commandId = 'workbench.action.chat.open';
@@ -227,41 +280,6 @@ export function activate(context: vscode.ExtensionContext) {
 			isPartialQuery: true
 		};
 		await vscode.commands.executeCommand(commandId, options);
-	}
-
-	function getFilePathFromEditor(): string | undefined {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			return editor.document.uri.fsPath;
-		}
-		// The editors showing an image file are custom editors. Therefore
-		// get the file path from the active tab
-		let tab = vscode.window.tabGroups.activeTabGroup.activeTab;
-		if (tab) {
-			if (tab.input instanceof vscode.TabInputCustom) {
-				return tab.input.uri.fsPath;
-			}
-		}
-		return undefined
-	}
-
-	function extractAllMarkdownCodeBlocks(markdown: string): string[] {
-		const codeBlockRegex = /```[\s\S]*?```/g;
-		const codeBlocks = markdown.match(codeBlockRegex);
-		return codeBlocks || [];
-	}
-
-	function getLanguageFromMarkdownCodeBlock(codeBlock: string): string | null {
-		const languageRegex = /```(\S*)[\s\S]*?```/;
-		const match = codeBlock.match(languageRegex);
-		return match && match[1] ? match[1] : null;
-	}
-
-	function removeFirstAndLastLine(text: string): string {
-		const lines = text.split('\n');
-		lines.shift();
-		lines.pop();
-		return lines.join('\n');
 	}
 }
 
